75e836e306d9cacc3388d089ddf2b11df82eef9a
emmanualtemp <emmanualtemp@gmail.com>
2025-02-01 21:17:12 +0530

    project setup added

---
+++ js/tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            backgroundImage: {
+                "banner-1": "url('/images/banner/banner-1.jpg')",
+            },
+            colors: {
+                "primary": "rgba(var(--primary-color),1)",
+                "primary-foreground": "rgba(var(--primary-foreground),1)",
+            },
+            translate: {
+
+            },
+            fontSize: {
+
+            },
+            height: {
+
+            },
+            width: {
+
+            }
+        }
+    }
+}
